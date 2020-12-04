2d57eaa4c4ae5bf12d9d8cc77c4ee8d2efb4ac2e
Moris R <moris.rafol@gmail.com>
2020-12-05 00:27:07 +0200

    feat:add notification click handler

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,24 @@
 self.addEventListener("push", function (e) {
-  const message = e.data.json();
-  self.registration.showNotification(message.text);
+    const data = e.data.json();
+    self.registration.showNotification(data.text, {data});
+
+});
+
+
+self.addEventListener('notificationclick', function (event) {
+    const url = `http://localhost:3000/treatment/${event.notification.data.treatmentId}`
+
+    event.waitUntil(
+        clients.matchAll({type: 'window'}).then( windowClients => {
+          for (var i = 0; i < windowClients.length; i++) {
+            var client = windowClients[i];
+            if (client.url === url && 'focus' in client) {
+              return client.focus();
+            }
+          }
+          if (clients.openWindow) {
+            return clients.openWindow(url);
+          }
+        })
+    );
 });
