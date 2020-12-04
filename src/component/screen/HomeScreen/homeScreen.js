66a9caf59ccbf38233a65fda4d27227f5edee906
Abeer Rizik <abeer.rizik93@gmail.com>
2020-12-04 22:39:23 +0200

    schdule

--- src/component/screen/HomeScreen/homeScreen.js
+++ src/component/screen/HomeScreen/homeScreen.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { login } from "../../../utils/login";
+import { getNurseSchedule } from "../../../utils/login";
 
 const HomeScreen = function () {
   const [schedule, setSchedule] = React.useState(null);
   React.useEffect(() => {
-    login()
+    getNurseSchedule()
       .then((data) => {
         console.log(data);
         setSchedule(data);
       })
       .catch(() => {});
   }, []);
-
+  console.log(schedule);
   return (
     <div className="main-card">
       <div className="title">
@@ -28,17 +28,21 @@ const HomeScreen = function () {
             <th>Status</th>
           </tr>
         </thead>
-        {/* <tbody>
-          {schedule.data.map(
-            ({ }) => (
-              <tr>
-                <td>{}</td>
-                <td>{}</td>
-                <td> {} </td>
-              </tr>
-            )
-          )}
-        </tbody> */}
+        <tbody>
+          {schedule?.map((data) => (
+            <tr key={data.id}>
+              <td>{data.Time}</td>
+              <td>{data["Patient Name"]}</td>
+              <td>{data.Room}</td>
+              <td>{data.Bed}</td>
+              <td>{data.Description}</td>
+              <td>
+                {" "}
+                <input type="checkbox" className="status" value="true" />{" "}
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
