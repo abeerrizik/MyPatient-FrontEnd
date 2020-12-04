42f9b2ec7b106872ffdd4c122ad44191b3f91f13
Moris R <moris.rafol@gmail.com>
2020-12-04 21:20:02 +0200

    fix: login error message not showing up

--- src/utils/login.js
+++ src/utils/login.js
@@ -1,12 +1,13 @@
 const { Axios, getUrl } = require("./backEndApiCall");
 
 export const login = async (id, password) => {
-  const response = await Axios.post(getUrl(`/login`), {
-    id,
-    password,
-  });
-  if (response.data.message) throw new Error(response.data.message);
-};
+    const response = await Axios.post(getUrl(`/login`), {
+        id,
+        password,
+    });
+    if (response.data.code !== 200)
+        throw new Error(response.data.message)
+}
 
 export const home = async () => {
   const response = await Axios.get(getUrl(`/nurse/schedule`));
